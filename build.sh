9992d1000ff5685da226f6c1cd796e2a1c59cbf9
Willi Püschel <wpueschel@gmail.com>
2022-03-09 19:23:05 +0100

    git clone moved out of the loop

--- build.sh
+++ build.sh
@@ -8,18 +8,23 @@ mkdir -p resource-types-tmp
 GOLANG_DOCKER_IMAGE_TAG=1.17.8-alpine3.15
 ALPINE_BASE_IMAGE_TAG=3.15.0
 
+# Clone the resources we want to be build into the worker
+git clone --depth 1 --branch v1.5.0 https://github.com/concourse/registry-image-resource.git resource-types-tmp/registry-image-resource
+git clone --depth 1 --branch v1.6.2 https://github.com/concourse/docker-image-resource resource-types-tmp/docker-image-resource
+git clone --depth 1 --branch v1.2.0 https://github.com/concourse/s3-resource.git resource-types-tmp/s3-resource
+git clone --depth 1 --branch v1.14.5  https://github.com/concourse/git-resource.git resource-types-tmp/git-resource
+
 RESOURCES="registry-image docker-image s3 git"
 
 for i in $RESOURCES; do
 
    # Clone the resources we want to be build into the worker
-   git clone --depth 1 --branch v1.5.0 https://github.com/concourse/${i}-resource.git resource-types-tmp/${i}-resource
    mkdir -p resource-types/$i
 
    # Build the docker images
    docker build -t ${i}-resource \
       --build-arg base-image=alpine:$ALPINE_BASE_IMAGE_TAG \
-      --build-arg builder_image=golang:$GOLANG_DOCKER_IMAGE_TAG \
+      --build-arg builder-image=golang:$GOLANG_DOCKER_IMAGE_TAG \
       -f resource-types-tmp/${i}-resource/dockerfiles/alpine/Dockerfile \
       resource-types-tmp/${i}-resource
 
